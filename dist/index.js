097ae05d148759862005511831815a7b6a4158c8
MohamedEl-Tabei <mohamedeltabei2000@gmail.com>
2025-01-21 20:22:02 +0200

    display video

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
-"use strict";
-console.log("test");
+import VideoPlayer from "./class/index.js";
+const videoPlayer = new VideoPlayer("container", "https://ia600207.us.archive.org/15/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4");
+console.log(videoPlayer);
--- src/class/index.ts
+++ src/class/index.ts
@@ -1,1 +1,20 @@
-import IVideoPlayer from "../interface";
+import IVideoPlayer from "../interface/index";
+class VideoPlayer implements IVideoPlayer{
+    readonly container: string;
+    readonly src: string;
+    constructor(_container:string,_src:string){
+        this.container=_container
+        this.src=_src
+        const video=document.createElement("video")
+        video.width=400;
+        video.height=320
+        video.autoplay=true
+
+        video.innerHTML=`
+            <source src=${this.src} type="video/mp4" />
+        `
+        document.getElementById("container")?.append(video)
+    }
+}
+
+export default VideoPlayer
