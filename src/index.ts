4676d99558bbc0af6e248e1f81a7fe443bfa9d34
MohamedEl-Tabei <mohamedeltabei2000@gmail.com>
2025-02-01 20:48:56 +0200

    update read me

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,27 @@ import VideoPlayer from "./class/index.js";
 const videoPlayer = new VideoPlayer(
   "container",
   "https://ia600207.us.archive.org/15/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-  "50%",
+  "500px",
   2,
   { color: "orange", backgroundColor: "black", accent: "orange" }
 );
 const videoPlayer1 = new VideoPlayer(
   "container",
   "https://ia600207.us.archive.org/15/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-  "50%",
+  "500px",
   4
 );
 const videoPlayer2 = new VideoPlayer(
   "container",
   "https://ia600207.us.archive.org/15/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-  "50%",
+  "500px",
   6,
   { color: "#2196f3", backgroundColor: "black", accent: "#2196f3" }
 );
 const videoPlayer3 = new VideoPlayer(
   "container",
   "https://ia600207.us.archive.org/15/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-  "50%",
+  "500px",
   8,
   { color: "#cddc39", backgroundColor: "black", accent: "#cddc39" }
 );
